b1b63a111e0d6d0e1e5b4a10d30e6989b68e49a5
Arte <jiangtao.cheng@orderplus.com>
2021-07-20 20:55:00 +0800

    init

---
+++ main_matrix.py
@@ -0,0 +1,31 @@
+from playLA.Matrix import Matrix
+from playLA.Vector import Vector
+
+if __name__ == "__main__":
+
+    matrix = Matrix([[1, 2], [3, 4]])
+    print(matrix)
+    print("matrix.shape = {}".format(matrix.shape()))
+    print("matrix.size = {}".format(matrix.size()))
+    print("len(matrix) = {}".format(len(matrix)))
+    print("matrix[0][0] = {}".format(matrix[0, 0]))
+    print(f"matrix[0][0] = {matrix[0, 0]}")
+
+    matrix2 = Matrix([[5, 6], [7, 8]])
+    print(f"add: {matrix + matrix2}")
+    print(f"subtract: {matrix - matrix2}")
+    print(f"scalar-mul: {matrix * 2}")
+    print(f"scalar-mul: {2 * matrix}")
+    print(f"zero_2_3: {Matrix.zero(2, 3)}")
+
+    T = Matrix([[1.5, 0], [0, 2]])
+    p = Vector([5, 3])
+    print(f"T.dot(p) = {T.dot(p)}")
+
+    P = Matrix([[0, 4, 5], [0, 0, 3]])
+    print(f"T.dot(P) = {T.dot(P)}")
+
+    print(f"A.dot(B) = {matrix.dot(matrix2)}")
+    print(f"B.dot(A) = {matrix2.dot(matrix)}")
+
+    print(f"P.T = {P.T()}")
---
+++ main_numpy_matrix.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+if __name__ == "__main__":
+
+    # 矩阵的创建
+    A = np.array([[1, 2], [3, 4]])
+    print(A)
+
+    # 矩阵的属性
+    print(A.shape)
+    print(A.T)
+
+    # 获取矩阵的元素
+    print(A[1, 1])
+    # 第一行
+    print(A[0])
+    # 第一列
+    print(A[:, 0])
+    # 第二行 等价于 A[1]
+    print(A[1, :])
+
+    # 矩阵的基本运算
+    B = np.array([[5, 6], [7, 8]])
+    print(A + B)
+    print(A - B)
+    print(10 * A)
+    print(A * 10)
+    # 对应位置的数相乘，无意义
+    print(A * B)
+    print(A.dot(B))
+
+    p = np.array([10, 100])
+    # 高维和低维运算，叫广播
+    print(A + p)
+    print(A + 1)
+
+    print(A.dot(p))
