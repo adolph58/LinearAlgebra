f4bbe898ec5c2ab2564976e3c70a484eb7f304ea
Arte <jiangtao.cheng@orderplus.com>
2021-08-03 21:26:26 +0800

    实现更一般化的高斯-约旦消元法

--- playLA/Vector.py
+++ playLA/Vector.py
@@ -1,4 +1,4 @@
-from ._global import EPSILON
+from ._global import is_zero
 import math
 
 class Vector:
@@ -17,7 +17,7 @@ class Vector:
 
     def normalize(self):
         """返回向量的单位向量"""
-        if self.norm() < EPSILON:
+        if is_zero(self.norm()):
             raise ZeroDivisionError("Normalize error! norm is zero.")
         return Vector(self._values) / self.norm()
 
